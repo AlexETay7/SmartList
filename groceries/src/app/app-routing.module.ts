ae6c0c82f6275b1d5481f7ac09b38bf3cdca7165
Alex Taylor <alexetaylor7@gmail.com>
2024-02-25 19:48:24 -0700

    added home page

--- groceries/src/app/app-routing.module.ts
+++ groceries/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    redirectTo: 'home', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'home', 
+    component: HomePage 
+  },
+  // Add other routes as needed
 ];
 
 @NgModule({
--- groceries/src/app/app.module.ts
+++ groceries/src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { GrocerylistComponent } from './components/grocerylist/grocerylist.component';
+import { HomePageModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent, GrocerylistComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, HomePageModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
