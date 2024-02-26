7e52d9967c542104753d67acad4ff38d13c86804
Alex Taylor <alexetaylor7@gmail.com>
2024-02-26 10:07:55 -0700

    added background images

--- groceries/src/app/home/home.page.ts
+++ groceries/src/app/home/home.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  backgroundImage: string = "";
+
   constructor() { }
 
   ngOnInit() {
+    const imagePaths = [
+      '../../assets/cake.jpg',
+      '../../assets/food-bowl.jpg',
+      '../../assets/pancakes.jpg',
+      '../../assets/pineapple.jpg',
+      '../../assets/pizza.jpg',
+      '../../assets/pretty-food.jpg',
+      '../../assets/salad.jpg'
+    ];
+
+    // Randomly select an image path
+    const randomIndex = Math.floor(Math.random() * imagePaths.length);
+    this.backgroundImage = imagePaths[randomIndex];
   }
 
 }
