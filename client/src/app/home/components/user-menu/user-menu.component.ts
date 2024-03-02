d77f40a6a32153db7c1c94922b652f88897a697d
Alex Taylor <alexetaylor7@gmail.com>
2024-03-01 23:24:45 -0700

    added page links + back buttons

--- client/src/app/home/components/user-menu/user-menu.component.ts
+++ client/src/app/home/components/user-menu/user-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-menu',
@@ -7,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserMenuComponent  implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {}
+
+  redirectMyToDoPage() {
+    this.router.navigate(['/mytodos']);
+  }
+
+  redirectToMyGroceriesPage() {
+    this.router.navigate(['/mygrocerylists']);
+  }
+
+  redirectToMyRecipesPage() {
+    this.router.navigate(['/myrecipes']);
+  }
+
+  redirectToHomePage() {
+    this.router.navigate(['/home']);
+  }
 
 }
